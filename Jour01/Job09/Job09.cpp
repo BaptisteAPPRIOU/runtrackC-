6b0082df26954c44ced19e03aba7bb3ed34c49ba
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-29 15:02:04 +0200

    Job10 done

---
+++ Jour01/Job09/Job09.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int a,b,c;
+    std::cout << "Enter a number: ";
+    std::cin >> a;
+    std::cout << "Enter a different second number: ";
+    std::cin >> b;
+    std::cout << "Enter a different third number: ";
+    std::cin >> c;
+    if (a > b && a > c) {
+        std::cout << a << " is the greatest number." << std::endl;
+    } else if (b > a && b > c) {
+        std::cout << b << " is the greatest number." << std::endl;
+    } else {
+        std::cout << c << " is the greatest number." << std::endl;
+    }
+}
