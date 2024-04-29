1b7a70c3229f5f9b86bc82c048ce3d952fb011c5
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-29 13:42:34 +0200

    Job00 done

--- Jour01/Job00/Job000.cpp
+++ Jour01/Job00/Job000.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
-    int a = 1 + 1;
-    cout << "The result is: " << endl;
-    return 0;
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    int x = 4;
+    int y = 5;
+    a = x+5;
+    cout << a << endl;
+    a = x=y +2;
+    cout << a << endl;
+    a = x == y;
+    cout << a << endl;
+    a = d++ * b+c;
+    cout << a << endl;
 }
