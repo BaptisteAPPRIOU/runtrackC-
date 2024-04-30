bc18c193fee75320cb915cc3327bb2fa640c0672
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-04-30 17:51:22 +0200

    Job01 done

---
+++ Jour03/Job01/Job01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main() {
+    std::string str ;
+    std::cout << "Entrez une phrase : ";
+    std::getline(std::cin, str);
+    std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+    std::cout << str << std::endl;
+    return 0;
+}
