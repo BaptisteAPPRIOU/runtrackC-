5dc10dd34808730effecfbc062f23b947bccb1c2
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-05-02 09:51:11 +0200

    Job05 done

--- Jour03/Job05/Job05.cpp
+++ Jour03/Job05/Job05.cpp
@@ -9,6 +9,16 @@ int main() {
         std::cout << "L'heure entrée n'est pas valide." << std::endl;
     } else if (chaine[2] != 'h') {
         std::cout << "L'heure entrée n'est pas valide." << std::endl;
+    } else if (chaine[0] < '0' || chaine[0] > '2') {
+        std::cout << "L'heure entrée n'est pas valide." << std::endl;
+    } else if (chaine[1] < '0' || chaine[1] > '9') {
+        std::cout << "L'heure entrée n'est pas valide." << std::endl;
+    } else if (chaine[3] < '0' || chaine[3] > '5') {
+        std::cout << "L'heure entrée n'est pas valide." << std::endl;
+    } else if (chaine[4] < '0' || chaine[4] > '9') {
+        std::cout << "L'heure entrée n'est pas valide." << std::endl;
+    } else if (chaine[0] == '2' && chaine[1] > '3') {
+        std::cout << "L'heure entrée n'est pas valide." << std::endl;
     } else {
         std::cout << "L'heure entrée est valide." << std::endl;
     }
